2d68c5c3757aefebe654dcaec3e6adf7907b0945
Alex-the-666 <alex.rowlands@cox.net>
2016-05-01 09:43:01 -0700

    Maybe Possibly solved #249

--- src/main/java/com/github/revival/server/handler/FossilEntities.java
+++ src/main/java/com/github/revival/server/handler/FossilEntities.java
@@ -36,28 +36,28 @@ public class FossilEntities {
     static int startEntityId = 300;
     
 	public static void registerEntities() {		
-		EntityRegistry.registerModEntity(EntityStoneboard.class, "StoneBoard", 1, Revival.INSTANCE, 250, Integer.MAX_VALUE, false);
-		EntityRegistry.registerModEntity(EntityJavelin.class, "Javelin", 2, Revival.INSTANCE, 250, 5, true);
-		EntityRegistry.registerModEntity(EntityAncientJavelin.class, "AncientJavelin", 3, Revival.INSTANCE, 250, 5, true);
-		EntityRegistry.registerModEntity(EntityMLighting.class, "FriendlyLighting", 4, Revival.INSTANCE, 250, 5, true);
+		EntityRegistry.registerModEntity(EntityStoneboard.class, "StoneBoard", 1, Revival.INSTANCE, 80, Integer.MAX_VALUE, false);
+		EntityRegistry.registerModEntity(EntityJavelin.class, "Javelin", 2, Revival.INSTANCE, 80, 3, true);
+		EntityRegistry.registerModEntity(EntityAncientJavelin.class, "AncientJavelin", 3, Revival.INSTANCE, 80, 3, true);
+		EntityRegistry.registerModEntity(EntityMLighting.class, "FriendlyLighting", 4, Revival.INSTANCE, 80, 3, true);
 		registerSpawnable(EntityFailuresaurus.class, "Failuresaurus", 5, 0x61ffbd, 0xf4eee6);
 		registerSpawnable(EntityBones.class, "Bones", 6, 12698049, 4802889);
-		EntityRegistry.registerModEntity(EntityDinoEgg.class, "DinoEgg", 8, Revival.INSTANCE, 250, 1, false);
-		EntityRegistry.registerModEntity(EntityFriendlyPigZombie.class, "FriendlyPigZombie", 12, Revival.INSTANCE, 250, 3, true);
-		EntityRegistry.registerModEntity(EntityDodoEgg.class, "DodoEgg", 26, Revival.INSTANCE, 250, 5, true);
+		EntityRegistry.registerModEntity(EntityDinoEgg.class, "DinoEgg", 8, Revival.INSTANCE, 80, 1, false);
+		EntityRegistry.registerModEntity(EntityFriendlyPigZombie.class, "FriendlyPigZombie", 12, Revival.INSTANCE, 80, 3, true);
+		EntityRegistry.registerModEntity(EntityDodoEgg.class, "DodoEgg", 26, Revival.INSTANCE, 80, 3, true);
 		//registerSpawnable(EntityCoelacanth.class, "Coelacanth", 28, 0X363941, 0X9BA1A9);
 		//registerSpawnable(EntityQuagga.class, "Quagga", 30, 0X763C24, 0XD3B9AB);
 		//registerSpawnable(EntityTerrorBird.class, "TerrorBird", 31, 0X346C5E, 0XF2EBD5);
-		EntityRegistry.registerModEntity(EntityTerrorBirdEgg.class, "TerrorBirdEgg", 32, Revival.INSTANCE, 250, 5, true);
-		EntityRegistry.registerModEntity(EntityAnuEffect.class, "AnuEffect", 34, Revival.INSTANCE, 250, 5, true);
+		EntityRegistry.registerModEntity(EntityTerrorBirdEgg.class, "TerrorBirdEgg", 32, Revival.INSTANCE, 80, 3, true);
+		EntityRegistry.registerModEntity(EntityAnuEffect.class, "AnuEffect", 34, Revival.INSTANCE, 80, 3, true);
 		registerSpawnable(EntityAnubite.class, "Anubite", 39, 0X2E1E14, 0X601200);
-		EntityRegistry.registerModEntity(EntityAnuDead.class, "AnuDead", 41, Revival.INSTANCE, 250, 3, true);
+		EntityRegistry.registerModEntity(EntityAnuDead.class, "AnuDead", 41, Revival.INSTANCE, 80, 3, true);
 		registerSpawnable(EntityTarSlime.class, "TarSlime", 99, 0X222222, 0x0B0B0B);
 		registerSpawnable(EntityAnu.class, "PigBoss", 101, 0X0F0F0F, 0XF72D00);
 		registerSpawnable(EntitySentryPigman.class, "SentryPigman", 102, 15373203, 0XD0A750);
-		EntityRegistry.registerModEntity(EntityToyBall.class, "ToyBall", 103, Revival.INSTANCE, 250, 5, true);
-		EntityRegistry.registerModEntity(EntityToyTetheredLog.class, "ToyTetheredLog", 104, Revival.INSTANCE, 250, 5, true);
-		EntityRegistry.registerModEntity(EntityToyScratchingPost.class, "ToyScratchingPost", 105, Revival.INSTANCE, 250, 5, true);
+		EntityRegistry.registerModEntity(EntityToyBall.class, "ToyBall", 103, Revival.INSTANCE, 80, 3, true);
+		EntityRegistry.registerModEntity(EntityToyTetheredLog.class, "ToyTetheredLog", 104, Revival.INSTANCE, 80, 3, true);
+		EntityRegistry.registerModEntity(EntityToyScratchingPost.class, "ToyScratchingPost", 105, Revival.INSTANCE, 80, 3, true);
 		for (int i = 0; i < EnumPrehistoric.values().length; i++) {
 			if(EnumPrehistoric.values()[i].type != EnumMobType.CHICKEN && EnumPrehistoric.values()[i].type != EnumMobType.VANILLA)
 				registerSpawnable(EnumPrehistoric.values()[i].getDinoClass(), EnumPrehistoric.values()[i].name(), 200 + i, EnumPrehistoric.values()[i].mainSpawnColor, EnumPrehistoric.values()[i].secondSpawnColor);
@@ -65,7 +65,7 @@ public class FossilEntities {
 	}
 
 	public static void registerSpawnable(Class entityClass, String name, int entityId, int mainColor, int subColor){
-		EntityRegistry.registerModEntity(entityClass, name, entityId, Revival.INSTANCE, 250, 1, true);
+		EntityRegistry.registerModEntity(entityClass, name, entityId, Revival.INSTANCE, 80, 3, true);
 		int id = getUniqueEntityId();
 		EntityList.IDtoClassMapping.put(id, entityClass);
 		EntityList.entityEggs.put(id, new EntityList.EntityEggInfo(id, mainColor, subColor));
