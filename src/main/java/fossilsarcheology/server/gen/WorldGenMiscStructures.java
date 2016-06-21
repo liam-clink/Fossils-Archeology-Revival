add3bd9b01a4b645484274a5376812fb77b357b0
Alex-the-666 <alex.rowlands@cox.net>
2016-06-21 12:37:21 -0700

    decrease structure spawns

--- src/main/java/fossilsarcheology/server/gen/WorldGenMiscStructures.java
+++ src/main/java/fossilsarcheology/server/gen/WorldGenMiscStructures.java
@@ -56,7 +56,7 @@ public class WorldGenMiscStructures implements IWorldGenerator {
 				}
 			}
 			if (Revival.CONFIG.generateTarSites) {
-				if (random.nextInt(45) == 0) {
+				if (random.nextInt(100) == 0) {
 					int Xcoord1 = chunkX * 16 + random.nextInt(16);
 					int Zcoord1 = chunkZ * 16 + random.nextInt(16);
 					int Ycoord1 = world.getHeightValue(Xcoord1, Zcoord1);
@@ -67,7 +67,7 @@ public class WorldGenMiscStructures implements IWorldGenerator {
 				}
 			}
 			if (Revival.CONFIG.generateFossilSites) {
-				if (random.nextInt(45) == 0) {
+				if (random.nextInt(100) == 0) {
 					int Xcoord1 = chunkX * 16 + random.nextInt(16);
 					int Zcoord1 = chunkZ * 16 + random.nextInt(16);
 					int Ycoord1 = world.getHeightValue(Xcoord1, Zcoord1);
