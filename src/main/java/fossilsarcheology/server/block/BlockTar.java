dbcfebbe12d6b3f0f98aaad8cd0b5305903913b3
Alex-the-666 <alex.rowlands@cox.net>
2016-06-22 17:43:05 -0700

    fixed velocity issue

--- src/main/java/fossilsarcheology/server/block/BlockTar.java
+++ src/main/java/fossilsarcheology/server/block/BlockTar.java
@@ -18,84 +18,86 @@ import net.minecraftforge.fluids.BlockFluidClassic;
 import java.util.Random;
 
 public class BlockTar extends BlockFluidClassic {
-    public static IIcon tar_still;
-    public static IIcon tar_flowing;
+	public static IIcon tar_still;
+	public static IIcon tar_flowing;
 
-    public BlockTar() {
-        super(FABlockRegistry.INSTANCE.tar_fluid, FABlockRegistry.INSTANCE.tar_material);
-        setTickRandomly(true);
-        setBlockName(LocalizationStrings.TAR_NAME);
-        setBlockTextureName("fossil:Tar");
-    }
+	public BlockTar() {
+		super(FABlockRegistry.INSTANCE.tar_fluid, FABlockRegistry.INSTANCE.tar_material);
+		setTickRandomly(true);
+		setBlockName(LocalizationStrings.TAR_NAME);
+		setBlockTextureName("fossil:Tar");
+	}
 
-    @Override
-    public void updateTick(World world, int x, int y, int z, Random rand) {
-        if (rand.nextInt(55) == 0) {
-            EntityTarSlime slime = new EntityTarSlime(world);
-            slime.setPositionAndRotation(x + 0.5D, y + 1D, z + 0.5, rand.nextInt(360), 0);
-            world.spawnEntityInWorld(slime);
-        }
-        super.updateTick(world, x, y, z, rand);
-    }
+	@Override
+	public void updateTick(World world, int x, int y, int z, Random rand) {
+		if (rand.nextInt(55) == 0) {
+			EntityTarSlime slime = new EntityTarSlime(world);
+			slime.setPositionAndRotation(x + 0.5D, y + 1D, z + 0.5, rand.nextInt(360), 0);
+			world.spawnEntityInWorld(slime);
+		}
+		super.updateTick(world, x, y, z, rand);
+	}
 
-    @Override
-    public boolean canDisplace(IBlockAccess world, int x, int y, int z) {
-        return !world.getBlock(x, y, z).getMaterial().isLiquid() && super.canDisplace(world, x, y, z);
-    }
+	@Override
+	public boolean canDisplace(IBlockAccess world, int x, int y, int z) {
+		return !world.getBlock(x, y, z).getMaterial().isLiquid() && super.canDisplace(world, x, y, z);
+	}
 
-    @Override
-    public boolean displaceIfPossible(World world, int x, int y, int z) {
-        return !world.getBlock(x, y, z).getMaterial().isLiquid() && super.displaceIfPossible(world, x, y, z);
-    }
+	@Override
+	public boolean displaceIfPossible(World world, int x, int y, int z) {
+		return !world.getBlock(x, y, z).getMaterial().isLiquid() && super.displaceIfPossible(world, x, y, z);
+	}
 
-    @Override
-    public void onEntityCollidedWithBlock(World world, int i, int j, int k, Entity entity) {
-        if (!(entity instanceof EntityTarSlime)) {
-            entity.setInWeb();
-            if (entity.getEyeHeight() + entity.posY >= j && entity.getEyeHeight() + entity.posY <= j + 1) {
-                entity.attackEntityFrom(DamageSource.drown, 1);
-            }
-        } else {
-            entity.setVelocity(0, 0.05, 0);
-        }
+	@Override
+	public void onEntityCollidedWithBlock(World world, int i, int j, int k, Entity entity) {
+		if (!(entity instanceof EntityTarSlime)) {
+			entity.setInWeb();
+			if (entity.getEyeHeight() + entity.posY >= j && entity.getEyeHeight() + entity.posY <= j + 1) {
+				entity.attackEntityFrom(DamageSource.drown, 1);
+			}
+		} else {
+			entity.motionX = 0D;
+			entity.motionY = 0.6D;
+			entity.motionZ = 0D;
+		}
 
-        if (entity instanceof EntitySheep) {
-            EntitySheep sheep = (EntitySheep) entity;
-            sheep.setFleeceColor(15);
-        }
-        if (entity instanceof EntitySkeleton) {
-            EntitySkeleton skele = (EntitySkeleton) entity;
-            skele.setSkeletonType(1);
-        }
-    }
+		if (entity instanceof EntitySheep) {
+			EntitySheep sheep = (EntitySheep) entity;
+			sheep.setFleeceColor(15);
+		}
+		if (entity instanceof EntitySkeleton) {
+			EntitySkeleton skele = (EntitySkeleton) entity;
+			skele.setSkeletonType(1);
+		}
+	}
 
-    @Override
-    public IIcon getIcon(int side, int meta) {
-        return (side == 0 || side == 1) ? tar_still : tar_flowing;
-    }
+	@Override
+	public IIcon getIcon(int side, int meta) {
+		return (side == 0 || side == 1) ? tar_still : tar_flowing;
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerBlockIcons(IIconRegister register) {
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerBlockIcons(IIconRegister register) {
 
-        tar_still = register.registerIcon("fossil:Tar");
-        tar_flowing = register.registerIcon("fossil:tar_flowing");
-    }
+		tar_still = register.registerIcon("fossil:Tar");
+		tar_flowing = register.registerIcon("fossil:tar_flowing");
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void randomDisplayTick(World world, int x, int y, int z, Random random) {
-        double var6 = (double) ((float) x + random.nextFloat());
-        double var8 = (double) y + 1 - maxY;
-        double var10 = (double) ((float) z + random.nextFloat());
-        FossilFX.spawnParticle("tarBubble", var6, var8 + 1, var10, 0.0D, 0.0D, 0.0D, 2);
-        if (random.nextInt(200) == 0) {
-            world.playSound(x, y, z, "fossil:tar", 0.1F + random.nextFloat() * 0.2F, 0.4F + random.nextFloat() * 0.15F, false);
-        }
-    }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void randomDisplayTick(World world, int x, int y, int z, Random random) {
+		double var6 = (double) ((float) x + random.nextFloat());
+		double var8 = (double) y + 1 - maxY;
+		double var10 = (double) ((float) z + random.nextFloat());
+		FossilFX.spawnParticle("tarBubble", var6, var8 + 1, var10, 0.0D, 0.0D, 0.0D, 2);
+		if (random.nextInt(200) == 0) {
+			world.playSound(x, y, z, "fossil:tar", 0.1F + random.nextFloat() * 0.2F, 0.4F + random.nextFloat() * 0.15F, false);
+		}
+	}
 
-    @Override
-    public boolean isNormalCube() {
-        return true;
-    }
+	@Override
+	public boolean isNormalCube() {
+		return true;
+	}
 }
