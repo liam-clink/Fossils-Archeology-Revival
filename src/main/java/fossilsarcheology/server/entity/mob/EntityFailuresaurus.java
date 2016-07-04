cb23e2c86c100b8bbe4b9b2ebb48ea58578c8bfe
Alex-the-666 <alex.rowlands@cox.net>
2016-07-03 19:33:26 -0700

    fix #296

--- src/main/java/fossilsarcheology/server/entity/mob/EntityFailuresaurus.java
+++ src/main/java/fossilsarcheology/server/entity/mob/EntityFailuresaurus.java
@@ -3,7 +3,10 @@ package fossilsarcheology.server.entity.mob;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
@@ -20,6 +23,9 @@ public class EntityFailuresaurus extends EntityMob {
 		this.setSize(0.8F, 0.8F);
 		this.experienceValue = 4;
 		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        this.tasks.addTask(3, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(4, new EntityAILookIdle(this));
 		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
 		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, true));
 
