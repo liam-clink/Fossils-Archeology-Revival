7cafa3fd86ff5c019ce10748e41f9d3a9359ec11
Alexthe666 <alex.rowlands@cox.net>
2016-07-10 08:44:15 -0700

    Get this dumb update Rolling

--- src/main/java/fossilsarcheology/server/gen/WorldGenMiscStructures.java
+++ src/main/java/fossilsarcheology/server/gen/WorldGenMiscStructures.java
@@ -40,7 +40,7 @@ public class WorldGenMiscStructures implements IWorldGenerator {
 				int Zcoord1 = chunkZ * 16 + random.nextInt(16);
 				int Ycoord1 = world.getHeightValue(Xcoord1, Zcoord1);
 				BiomeGenBase biome = world.getWorldChunkManager().getBiomeGenAt(Xcoord1, Zcoord1);
-				if (BiomeDictionary.isBiomeOfType(biome, Type.BEACH) && !world.provider.hasNoSky && !world.provider.isHellWorld && biome.topBlock == Blocks.grass && world.getBlock(Xcoord1, Ycoord1 - 1, Zcoord1) == biome.topBlock) {
+				if (BiomeDictionary.isBiomeOfType(biome, Type.BEACH) && !world.provider.hasNoSky && !world.provider.isHellWorld && world.getBlock(Xcoord1, Ycoord1 - 1, Zcoord1) == Blocks.sand) {
 					new MoaiWorldGen().generate(world, random, Xcoord1, Ycoord1 - random.nextInt(4), Zcoord1);
 				}
 			}
