5978ddbe4247e92b0ac5232a1a0b6fdd67d25cc7
Absinthe <Sunconure11@users.noreply.github.com>
2016-07-23 10:29:33 -0400

    Project Zulu support (#438)

--- src/main/java/fossilsarcheology/server/util/FossilFoodMappings.java
+++ src/main/java/fossilsarcheology/server/util/FossilFoodMappings.java
@@ -163,6 +163,10 @@ public class FossilFoodMappings {
         FoodMappings.INSTANCE.addPlant("cropCassis", 7);
         FoodMappings.INSTANCE.addPlant("treeLeaves", 20);
         FoodMappings.INSTANCE.addPlant("treeSapling", 15);
+        FoodMappings.INSTANCE.addPlant("sapling", 15);
+        FoodMappings.INSTANCE.addPlant("saplingPalm", 15);   
+        FoodMappings.INSTANCE.addPlant("leaves", 20);
+        FoodMappings.INSTANCE.addPlant("leavesPalm", 20);
         FoodMappings.INSTANCE.addPlant("listAllgrain", 10);
         FoodMappings.INSTANCE.addPlant("cropRice", 10);
         FoodMappings.INSTANCE.addPlant("cropCorn", 10);
