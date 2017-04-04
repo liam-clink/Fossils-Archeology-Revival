a69f70891b545a6decd374e918b948925d96363c
Alex-the-666 <alex.rowlands@cox.net>
2017-04-04 15:17:08 -0700

    old assets plus f u quagga you peace of poop

---
+++ src/main/java/fossilsarcheology/server/entity/prehistoric/EntityQuagga.java
@@ -0,0 +1,30 @@
+package fossilsarcheology.server.entity.prehistoric;
+
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class EntityQuagga extends Entity {
+
+    public EntityQuagga(World par1World) {
+        super(par1World);
+
+    }
+
+    @Override
+    protected void entityInit() {
+
+    }
+
+    @Override
+    protected void readEntityFromNBT(NBTTagCompound compound) {
+
+    }
+
+    @Override
+    protected void writeEntityToNBT(NBTTagCompound compound) {
+
+    }
+
+}
