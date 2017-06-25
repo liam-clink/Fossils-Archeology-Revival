5bd3ab1510678d8c384cf7ca8541bb79f78f22e4
Alex-the-666 <alex.rowlands@cox.net>
2017-06-25 14:24:10 -0700

    sarcophagus

--- src/main/java/fossilsarcheology/server/block/SarcophagusBlock.java
+++ src/main/java/fossilsarcheology/server/block/SarcophagusBlock.java
@@ -1,6 +1,7 @@
 package fossilsarcheology.server.block;
 
-import fossilsarcheology.server.block.entity.TileEntityAnubiteStatue;
+import fossilsarcheology.server.block.entity.TileEntitySarcophagus;
+import fossilsarcheology.server.item.FAItemRegistry;
 import fossilsarcheology.server.tab.FATabRegistry;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
@@ -9,10 +10,11 @@ import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumBlockRenderType;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -20,16 +22,47 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class AnubiteStatueBlock extends BlockContainer {
+import javax.annotation.Nullable;
+
+public class SarcophagusBlock extends BlockContainer {
     public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
-    protected AnubiteStatueBlock() {
+    public SarcophagusBlock() {
         super(Material.ROCK);
         this.setCreativeTab(FATabRegistry.BLOCKS);
         this.setTickRandomly(true);
         this.setBlockUnbreakable();
         this.setResistance(60000000.0F);
-        this.setUnlocalizedName("anubiteStatue");
+        this.setUnlocalizedName("sarcophagus");
+
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ){
+        TileEntitySarcophagus tile = (TileEntitySarcophagus) world.getTileEntity(pos);
+        if (tile.chestState == 0) {
+            if (player.getHeldItem(hand) != null) {
+                if (player.getHeldItem(hand).getItem() != null) {
+                    if (player.getHeldItem(hand).getItem() == FAItemRegistry.SCARAB_GEM) {
+                        tile.chestState = 1;
+                        if (!player.capabilities.isCreativeMode) {
+                            --player.getHeldItem(hand).stackSize;
+                        }
+
+                        if (player.getHeldItem(hand).stackSize <= 0) {
+                            player.inventory.setInventorySlotContents(player.inventory.currentItem, null);
+                        }
+
+                    }
+                }
+            }
+        } else if (tile.chestState == 1) {
+            tile.chestState = 2;
+            tile.chestLidCounter = 1;
+            world.playSound(player, pos, SoundEvents.BLOCK_CHEST_OPEN, SoundCategory.BLOCKS, 0.5F, world.rand.nextFloat() * 0.1F + 0.9F);
+
+        }
+        return true;
     }
 
     public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
@@ -69,6 +102,6 @@ public class AnubiteStatueBlock extends BlockContainer {
 
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
-        return new TileEntityAnubiteStatue();
+        return new TileEntitySarcophagus();
     }
 }
