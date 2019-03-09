68b4331ac1efe920de4579ca3ae888dc8af8c393
Alexthe666 <alex.rowlands@cox.net>
2019-03-09 13:59:45 -0800

    fix #1270

--- src/main/java/fossilsarcheology/server/block/TallFlowerBlock.java
+++ src/main/java/fossilsarcheology/server/block/TallFlowerBlock.java
@@ -2,10 +2,7 @@ package fossilsarcheology.server.block;
 
 import fossilsarcheology.server.api.DefaultRenderedItem;
 import fossilsarcheology.server.tab.FATabRegistry;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockBush;
-import net.minecraft.block.IGrowable;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
@@ -13,6 +10,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
@@ -27,147 +26,148 @@ import java.util.Random;
 
 public class TallFlowerBlock extends BlockBush implements DefaultRenderedItem, IGrowable {
 
-	public static final PropertyEnum<TallFlowerBlock.EnumBlockHalf> HALF = PropertyEnum.create("half", TallFlowerBlock.EnumBlockHalf.class);
+    public static final PropertyEnum<TallFlowerBlock.EnumBlockHalf> HALF = PropertyEnum.create("half", TallFlowerBlock.EnumBlockHalf.class);
 
-	public TallFlowerBlock(String name) {
-		super(Material.PLANTS);
-		this.setTickRandomly(true);
-		this.setTranslationKey(name);
-		this.setSoundType(SoundType.PLANT);
-		this.setCreativeTab(FATabRegistry.BLOCKS);
-		this.setDefaultState(this.blockState.getBaseState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.LOWER));
-	}
+    public TallFlowerBlock(String name) {
+        super(Material.PLANTS);
+        this.setTickRandomly(true);
+        this.setTranslationKey(name);
+        this.setSoundType(SoundType.PLANT);
+        this.setCreativeTab(FATabRegistry.BLOCKS);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.LOWER));
+    }
 
-	@Override
-	public boolean isFlammable(IBlockAccess world, BlockPos pos, EnumFacing face) {
-		return true;
-	}
+    @Override
+    public boolean isFlammable(IBlockAccess world, BlockPos pos, EnumFacing face) {
+        return true;
+    }
 
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return new AxisAlignedBB(0.30000001192092896D, 0.0D, 0.30000001192092896D, 0.699999988079071D, 1D, 0.699999988079071D);
-	}
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return new AxisAlignedBB(0.30000001192092896D, 0.0D, 0.30000001192092896D, 0.699999988079071D, 1D, 0.699999988079071D);
+    }
 
-	@Override
-	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
-		return super.canPlaceBlockAt(worldIn, pos) && worldIn.isAirBlock(pos.up());
-	}
+    @Override
+    public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
+        return super.canPlaceBlockAt(worldIn, pos) && worldIn.isAirBlock(pos.up());
+    }
 
-	@Override
-	protected void checkAndDropBlock(World world, BlockPos pos, IBlockState state) {
-		if (state.getBlock() == this && !this.canBlockStay(world, pos, state)) {
-			boolean flag = state.getValue(HALF) == TallFlowerBlock.EnumBlockHalf.UPPER;
-			BlockPos blockpos = flag ? pos : pos.up();
-			BlockPos blockpos1 = flag ? pos.down() : pos;
-			Block block = flag ? this : world.getBlockState(blockpos).getBlock();
-			Block block1 = flag ? world.getBlockState(blockpos1).getBlock() : this;
+    @Override
+    protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
+        if (!this.canBlockStay(worldIn, pos, state)) {
+            boolean flag = state.getValue(HALF) == EnumBlockHalf.UPPER;
+            BlockPos blockpos = flag ? pos : pos.up();
+            BlockPos blockpos1 = flag ? pos.down() : pos;
+            Block block = (Block) (flag ? this : worldIn.getBlockState(blockpos).getBlock());
+            Block block1 = (Block) (flag ? worldIn.getBlockState(blockpos1).getBlock() : this);
 
-			if (!flag) {
-				this.dropBlockAsItem(world, pos, state, 0);
-			}
+            if (!flag) this.dropBlockAsItem(worldIn, pos, state, 0); //Forge move above the setting to air.
 
-			if (block == this) {
-				world.setBlockState(blockpos, Blocks.AIR.getDefaultState(), 2);
-			}
+            if (block == this) {
+                worldIn.setBlockState(blockpos, Blocks.AIR.getDefaultState(), 2);
+            }
 
-			if (block1 == this) {
-				world.setBlockState(blockpos1, Blocks.AIR.getDefaultState(), 3);
-			}
-		}
-	}
+            if (block1 == this) {
+                worldIn.setBlockState(blockpos1, Blocks.AIR.getDefaultState(), 3);
+            }
+        }
+    }
 
-	@Override
-	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
-		if (state.getValue(HALF) == TallFlowerBlock.EnumBlockHalf.UPPER) {
-			//worldIn.setBlockState(pos.down(), this.getDefaultState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.LOWER), 2);
-			return worldIn.getBlockState(pos.down()).getBlock() == this;
-		} else {
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        if (state.getValue(HALF) == EnumBlockHalf.UPPER) {
+            return Items.AIR;
+        }else{
+            return super.getItemDropped(state, rand, fortune);
+        }
+    }
 
-			IBlockState iblockstate = worldIn.getBlockState(pos.up());
-			return iblockstate.getBlock() == this && super.canBlockStay(worldIn, pos, iblockstate);
-		}
-	}
+    @Override
+    public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
+        if (state.getValue(HALF) == TallFlowerBlock.EnumBlockHalf.UPPER) {
+            //worldIn.setBlockState(pos.down(), this.getDefaultState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.LOWER), 2);
+            return worldIn.getBlockState(pos.down()).getBlock() == this;
+        } else {
 
-	@Override
-	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-		worldIn.setBlockState(pos, this.getDefaultState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.LOWER), 2);
-		worldIn.setBlockState(pos.up(), this.getDefaultState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.UPPER), 2);
-	}
+            IBlockState iblockstate = worldIn.getBlockState(pos.up());
+            return iblockstate.getBlock() == this && super.canBlockStay(worldIn, pos, iblockstate);
+        }
+    }
 
-	@Override
-	public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player) {
-		if (state.getValue(HALF) == TallFlowerBlock.EnumBlockHalf.UPPER) {
-			if (worldIn.getBlockState(pos.down()).getBlock() == this) {
-				if (!player.capabilities.isCreativeMode) {
-					IBlockState iblockstate = worldIn.getBlockState(pos.down());
-					worldIn.destroyBlock(pos.down(), true);
-				} else {
-					worldIn.setBlockToAir(pos.down());
-				}
-			}
-		} else if (worldIn.getBlockState(pos.up()).getBlock() == this) {
-			worldIn.setBlockState(pos.up(), Blocks.AIR.getDefaultState(), 2);
-		}
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        worldIn.setBlockState(pos, this.getDefaultState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.LOWER), 2);
+        worldIn.setBlockState(pos.up(), this.getDefaultState().withProperty(HALF, TallFlowerBlock.EnumBlockHalf.UPPER), 2);
+    }
 
-		super.onBlockHarvested(worldIn, pos, state, player);
-	}
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player) {
+        if (state.getValue(HALF) == TallFlowerBlock.EnumBlockHalf.UPPER) {
+            if (worldIn.getBlockState(pos.down()).getBlock() == this) {
+                worldIn.setBlockToAir(pos.down());
+            }
+        } else if (worldIn.getBlockState(pos.up()).getBlock() == this) {
+            worldIn.setBlockState(pos.up(), Blocks.AIR.getDefaultState(), 2);
+        }
 
-	@Override
-	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
-		return true;
-	}
+        super.onBlockHarvested(worldIn, pos, state, player);
+    }
 
-	@Override
-	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-		return true;
-	}
+    @Override
+    public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
+        return true;
+    }
 
-	@Override
-	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-		spawnAsEntity(worldIn, pos, new ItemStack(this));
-	}
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        return true;
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public IBlockState getStateFromMeta(int meta) {
-		return this.getDefaultState().withProperty(HALF, meta == 1 ? EnumBlockHalf.UPPER : EnumBlockHalf.LOWER);
-	}
+    @Override
+    public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        spawnAsEntity(worldIn, pos, new ItemStack(this));
+    }
 
-	@Override
-	public int getMetaFromState(IBlockState state) {
-		return state.getValue(HALF) == TallFlowerBlock.EnumBlockHalf.UPPER ? 1 : 0;
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(HALF, meta == 1 ? EnumBlockHalf.UPPER : EnumBlockHalf.LOWER);
+    }
 
-	@Override
-	protected BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, HALF);
-	}
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(HALF) == TallFlowerBlock.EnumBlockHalf.UPPER ? 1 : 0;
+    }
 
-	@Override
-	public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
-		if (state.getBlock() == this && state.getValue(HALF) == EnumBlockHalf.LOWER && world.getBlockState(pos.up()).getBlock() == this) {
-			world.setBlockToAir(pos.up());
-		}
-		return world.setBlockToAir(pos);
-	}
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, HALF);
+    }
 
-	@Override
-	@SideOnly(Side.CLIENT)
-	public Block.EnumOffsetType getOffsetType() {
-		return Block.EnumOffsetType.XZ;
-	}
+    @Override
+    public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
+        if (state.getBlock() == this && state.getValue(HALF) == EnumBlockHalf.LOWER && world.getBlockState(pos.up()).getBlock() == this) {
+            world.setBlockToAir(pos.up());
+        }
+        return world.setBlockToAir(pos);
+    }
 
-	public enum EnumBlockHalf implements IStringSerializable {
-		UPPER, LOWER;
+    @Override
+    @SideOnly(Side.CLIENT)
+    public Block.EnumOffsetType getOffsetType() {
+        return Block.EnumOffsetType.XZ;
+    }
 
-		@Override
-		public String toString() {
-			return this.getName();
-		}
+    public enum EnumBlockHalf implements IStringSerializable {
+        UPPER, LOWER;
 
-		@Override
-		public String getName() {
-			return this == UPPER ? "upper" : "lower";
-		}
-	}
+        @Override
+        public String toString() {
+            return this.getName();
+        }
+
+        @Override
+        public String getName() {
+            return this == UPPER ? "upper" : "lower";
+        }
+    }
 }
