e20c86b383280205892f67f17f3822969221b00f
Sunconure11 <eyespot7b@aol.com>
2019-05-05 15:39:47 -0400

    Gave aspects to Tiktaalik and Platybelodon

--- src/main/java/fossilsarcheology/server/compat/thaumcraft/ThaumcraftCompat.java
+++ src/main/java/fossilsarcheology/server/compat/thaumcraft/ThaumcraftCompat.java
@@ -227,6 +227,9 @@ public class ThaumcraftCompat {
 		ThaumcraftApi.registerEntityTag("fossil.sentrypigman", new AspectList().add(Aspect.SOUL, 30).add(Aspect.DESIRE, 30).add(Aspect.UNDEAD, 30).add(Aspect.FIRE, 30).add(Aspect.DARKNESS, 30));
 		ThaumcraftApi.registerEntityTag("fossil.friendlypigman", new AspectList().add(Aspect.SOUL, 30).add(Aspect.DESIRE, 30).add(Aspect.UNDEAD, 30).add(Aspect.FIRE, 30).add(Aspect.DARKNESS, 30));
 
+		ThaumcraftApi.registerEntityTag("fossil.tikaalik", new AspectList().add(Aspect.BEAST, 25).add(Aspect.EARTH, 25).add(Aspect.WATER, 25).add(Aspect.EXCHANGE, 15).add(ThaumcraftCompat.TIME, 20).add(ThaumcraftCompat.FOSSIL, 20));
+		ThaumcraftApi.registerEntityTag("fossil.platybelodon", new AspectList().add(Aspect.BEAST, 35).add(Aspect.WATER, 35).add(Aspect.TOOL, 35).add(ThaumcraftCompat.TIME, 20).add(ThaumcraftCompat.FOSSIL, 20));
+
 		ThaumcraftApi.registerEntityTag("fossil.megalania", new AspectList().add(Aspect.BEAST, 35).add(Aspect.AVERSION, 35).add(Aspect.ALCHEMY, 35).add(ThaumcraftCompat.TIME, 20).add(ThaumcraftCompat.FOSSIL, 20));
 		ThaumcraftApi.registerEntityTag("fossil.meganeura", new AspectList().add(Aspect.BEAST, 35).add(Aspect.AVERSION, 35).add(Aspect.WATER, 35).add(Aspect.FLIGHT, 35).add(ThaumcraftCompat.TIME, 20).add(ThaumcraftCompat.FOSSIL, 20));
 		ThaumcraftApi.registerEntityTag("fossil.megaloceros", new AspectList().add(Aspect.BEAST, 35).add(Aspect.AVERSION, 35).add(Aspect.EARTH, 35).add(ThaumcraftCompat.TIME, 20).add(ThaumcraftCompat.FOSSIL, 20));
