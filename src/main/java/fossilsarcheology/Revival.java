596a881a2fe410f259b81c6c81beb9281d2b7376
Alexthe666 <alex.rowlands@cox.net>
2019-07-04 15:00:57 -0700

    8.0.2

--- src/main/java/fossilsarcheology/Revival.java
+++ src/main/java/fossilsarcheology/Revival.java
@@ -31,7 +31,7 @@ import java.io.File;
 
 public class Revival {
     public static final String MODID = "fossil";
-    public static final String VERSION = "8.0.1";
+    public static final String VERSION = "8.0.2";
     public static final ReleaseType RELEASE_TYPE = ReleaseType.parseVersion(VERSION);
     public static final String LLIBRARY_VERSION = "1.7.17";
 
