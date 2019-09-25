9596fef09aa489d0282f4b5e017879a315d17911
Alexthe666 <alex.rowlands@cox.net>
2019-09-25 11:29:13 -0700

    upped tooth dagger attack speed

--- src/main/java/fossilsarcheology/server/item/BasicSwordItem.java
+++ src/main/java/fossilsarcheology/server/item/BasicSwordItem.java
@@ -1,5 +1,6 @@
 package fossilsarcheology.server.item;
 
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import fossilsarcheology.server.api.DefaultRenderedItem;
 import fossilsarcheology.server.tab.FATabRegistry;
@@ -22,14 +23,12 @@ public class BasicSwordItem extends ItemSword implements DefaultRenderedItem {
 	@Override
     public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot) {
 		if (this == FAItemRegistry.TOOTH_DAGGER) {
-			Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(equipmentSlot);
-
+			Multimap<String, AttributeModifier> multimap = HashMultimap.<String, AttributeModifier>create();
 			if (equipmentSlot == EntityEquipmentSlot.MAINHAND) {
-				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double) this.getAttackDamage(), 0));
-				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -10.4000000953674316D, 0));
+				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", 5D, 0));
+				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", 4D, 0));
 			}
 			return multimap;
-
 		} else {
 			return super.getItemAttributeModifiers(equipmentSlot);
 		}
