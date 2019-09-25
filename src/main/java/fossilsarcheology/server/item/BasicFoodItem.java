cec2e2ca22a0e396a41d61594f618cb29b58beae
Alexthe666 <alex.rowlands@cox.net>
2019-09-24 20:55:34 -0700

    chicken soup gives you the bucket back now

--- src/main/java/fossilsarcheology/server/item/BasicFoodItem.java
+++ src/main/java/fossilsarcheology/server/item/BasicFoodItem.java
@@ -2,12 +2,23 @@ package fossilsarcheology.server.item;
 
 import fossilsarcheology.server.api.DefaultRenderedItem;
 import fossilsarcheology.server.tab.FATabRegistry;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class BasicFoodItem extends ItemFood implements DefaultRenderedItem {
 	public BasicFoodItem(int amount, float saturation, boolean meat, String name) {
 		super(amount, saturation, meat);
 		this.setCreativeTab(FATabRegistry.ITEMS);
 		this.setTranslationKey(name);
+	}
+
+	protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player){
+		if(stack.getItem() == FAItemRegistry.RAW_CHICKEN_SOUP || stack.getItem() == FAItemRegistry.COOKED_CHICKEN_SOUP){
+			player.addItemStackToInventory(new ItemStack(Items.BUCKET));
+		}
 	}
 }
